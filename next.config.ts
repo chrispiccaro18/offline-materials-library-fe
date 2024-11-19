b29f366a9b69c4834d4fbbcbeebb598233245172
Chris Piccaro <chrispiccaro18@gmail.com>
2024-11-18 23:06:34 -0500

    first commit, monorepo - need to detangle

---
+++ next.config.ts
@@ -0,0 +1,38 @@
+import type { NextConfig } from "next";
+// import next from "next";
+import withPWA from "next-pwa";
+
+const nextConfig: NextConfig = {
+  // Your existing Next.js options
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+const pwaConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+      },
+    },
+  ],
+});
+
+const mergedConfig: NextConfig = {
+  ...pwaConfig,
+  ...nextConfig,
+};
+
+// Export the merged configuration
+export default mergedConfig;
+
